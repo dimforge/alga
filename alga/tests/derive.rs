17b22c709ba3c7716fc49fb468ac86fe663548ed
Wadelma <delma@del.ma>
2017-04-02 02:07:30 +0300

    Start of implementing quickcheck via custom derive.

--- alga/tests/derive.rs
+++ alga/tests/derive.rs
@@ -1,12 +1,50 @@
 extern crate alga;
 #[macro_use]
 extern crate alga_derive;
+extern crate quickcheck;
+extern crate approx;
 
 use alga::general::{Inverse, Identity, Additive, Multiplicative, AbstractMagma};
 
-#[derive(Alga, Clone, PartialEq)]
+use approx::ApproxEq;
+
+use quickcheck::{Arbitrary, Gen};
+
+#[derive(Alga, Clone, PartialEq, Debug)]
 #[alga_traits(Field(Additive, Multiplicative))]
 struct W(f64);
+
+impl ApproxEq for W {
+    type Epsilon = W;
+    fn default_epsilon() -> W {
+        W(f64::default_epsilon())
+    }
+
+    fn default_max_relative() -> W {
+        W(f64::default_max_relative())
+    }
+
+    fn default_max_ulps() -> u32 {
+        f64::default_max_ulps()
+    }
+
+    fn relative_eq(&self, other: &Self, epsilon: W, max_relative: W) -> bool {
+        self.0.relative_eq(&other.0, epsilon.0, max_relative.0)
+    }
+
+    fn ulps_eq(&self, other: &Self, epsilon: W, max_ulps: u32) -> bool {
+        self.0.ulps_eq(&other.0, epsilon.0, max_ulps)
+    }
+}
+
+impl Arbitrary for W {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        W(f64::arbitrary(g))
+    }
+    fn shrink(&self) -> Box<Iterator<Item=Self>> {
+        Box::new(self.0.shrink().map(W))
+    }
+}
 
 impl AbstractMagma<Additive> for W {
     fn operate(&self, right: &Self) -> Self {
