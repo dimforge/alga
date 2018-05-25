f1db48f5abebb2d2d70296309297d912ac58831b
sebcrozet <developer@crozet.re>
2018-05-25 09:29:14 +0200

    CI: compile for wasm32 as well.

--- ci/build.sh
+++ ci/build.sh
@@ -7,6 +7,9 @@ DIR=`pwd`;
 if [ -z "$NO_STD" ]; then
     cd "$DIR/alga" && cargo build --verbose;
     cd "$DIR/alga_derive" && cargo build --verbose;
+    rustup target install wasm32-unknown-unknown
+    cd "$DIR/alga" && cargo build --verbose --target wasm32-unknown-unknown;
+    cd "$DIR/alga_derive" && cargo build --verbose --target wasm32-unknown-unknown;
     if [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
         cd "$DIR/alga_derive_test" && cargo build --verbose;
     fi
